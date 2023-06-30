7f8942c5a9133dccfe351fb67b2bdfbbc55baec0
YC <lychgxnn@outlook.com>
2023-06-30 12:19:45 +0100

    use class

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
-students = {}
-teachers_subject = {}
-teachers_cls = {}
-homeroom_teachers = {}
+
+class Student:
+    def __init__(self, first_name, last_name, class_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.class_name = class_name
+
+class Teacher:
+    def __init__(self, first_name, last_name, subject, classes):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.subject = subject
+        self.classes = classes
+
+class HomeroomTeacher:
+    def __init__(self, first_name, last_name, class_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.class_name = class_name
+
+students = []
+teachers = []
+homeroom_teachers = []
 
 def create_user():
     user_type = input("Enter the user type (student, teacher, homeroom teacher, end): ")
 
     if user_type == "student":
-        student_name = input("Enter the student's name: ")
-        class_name = input("Enter the class name: ")
-        students[student_name] = class_name
+        first_name = input("Enter student's first name: ")
+        last_name = input("Enter student's last name: ")
+        class_name = input("Enter student's class name: ")
+        student = Student(first_name, last_name, class_name)
+        students.append(student)
+
     elif user_type == "teacher":
-        teacher_name = input("Enter the teacher's name: ")
-        subject = input("Enter a subject: ")
-        teachers_subject[teacher_name] = subject
-        clss = []
+        first_name = input("Enter teacher's first name: ")
+        last_name = input("Enter teacher's last name: ")
+        subject = input("Enter the subject taught by the teacher: ")
+        classes = []
         while True:
-            cls = input("Enter classes this teacher teach (leave empty to finish): ")
-            if not cls:
+            class_name = input("Enter a class taught by the teacher (or type end to finish): ")
+            if class_name == "end":
                 break
-            clss.append(cls)
-        teachers_cls[teacher_name] = clss
+            classes.append(class_name)
+        teacher = Teacher(first_name, last_name, subject, classes)
+        teachers.append(teacher)
 
     elif user_type == "homeroom teacher":
-        teacher_name = input("Enter the homeroom teacher's name: ")
-        class_name = input("Enter the class name: ")
-        homeroom_teachers[teacher_name] = class_name
+        first_name = input("Enter homeroom teacher's first name: ")
+        last_name = input("Enter homeroom teacher's last name: ")
+        class_name = input("Enter the class led by the homeroom teacher: ")
+        homeroom_teacher = HomeroomTeacher(first_name, last_name, class_name)
+        homeroom_teachers.append(homeroom_teacher)
+
     elif user_type == "end":
         return
 
@@ -35,40 +61,43 @@ def manage_users():
     if manage_option == "class":
         class_name = input("Enter the class name: ")
         print("Students in the class:")
-        for student, cls_name in students.items():
-            if cls_name == class_name:
-                print(student)
+        for student in students:
+            if student.class_name == class_name:
+                print(f"{student.first_name} {student.last_name}")
         print("Homeroom teacher:")
-        for teacher, cls_name in homeroom_teachers.items():
-            if cls_name == class_name:
-                print(teacher)
+        for homeroom_teacher in homeroom_teachers:
+            if homeroom_teacher.class_name == class_name:
+                print(f"{homeroom_teacher.first_name} {homeroom_teacher.last_name}")
 
     elif manage_option == "student":
-        student_name = input("Enter the student's name: ")
-        print("Classes attended by the student:")
-        for student, cls_name in students.items():
-            if student == student_name:
-                print(cls_name)
-        print("Teachers of the classes:")
-        for teacher, classes in teachers_cls.items():
-            if students.get(student_name) in classes:
-                print(teacher)
+        first_name = input("Enter student's first name: ")
+        last_name = input("Enter student's last name: ")
+        for student in students:
+            if student.first_name == first_name and student.last_name == last_name:
+                print("Class of the student:")
+                print(student.class_name)
+                for teacher in teachers:
+                    if student.class_name in teacher.classes:
+                        print(f"{teacher.first_name} {teacher.last_name}. Subject:{teacher.subject}")
 
     elif manage_option == "teacher":
-        teacher_name = input("Enter the teacher's name: ")
+        first_name = input("Enter teacher's first name: ")
+        last_name = input("Enter teacher's last name: ")
         print("Classes taught by the teacher:")
-        for teacher, classes in teachers_cls.items():
-            if teacher == teacher_name:
-                print(classes)
+        for teacher in teachers:
+            if teacher.first_name == first_name and teacher.last_name == last_name:
+                for class_name in teacher.classes:
+                    print(class_name)
 
     elif manage_option == "homeroom teacher":
-        teacher_name = input("Enter the homeroom teacher's name: ")
+        first_name = input("Enter homeroom teacher's first name: ")
+        last_name = input("Enter homeroom teacher's last name: ")
         print("Students led by the homeroom teacher:")
-        for teacher, cls_name in homeroom_teachers.items():
-            if teacher == teacher_name:
-                for student, cls_name in students.items():
-                    if cls_name == cls_name:
-                        print(student)
+        for homeroom_teacher in homeroom_teachers:
+            if homeroom_teacher.first_name == first_name and homeroom_teacher.last_name == last_name:
+                for student in students:
+                    if student.class_name == homeroom_teacher.class_name:
+                        print(f"{student.first_name} {student.last_name}")
 
     elif manage_option == "end":
         return
